ab7905ba9d9696ab5a2d98fd5d38c999a556dbe9
ej838639 <eric.johnson838639@gmail.com>
2023-02-03 16:26:19 -0800

    devops: LAZ-8 terraform2 folder created with main.tf to create an EC2 instance

---
+++ terraform2/main.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-west-2"
+}
+
+locals {
+  smallest_instance = "t2.micro"
+  amazon_linux_os = "ami-06e85d4c3149db26a"
+}
+
+resource "aws_instance" "lazarus" {
+  ami           = local.amazon_linux_os
+  instance_type = local.smallest_instance
+
+  tags = {
+    Name = "lazarus"
+  }
+}
+
+output "instance_public_ip" {
+  value = aws_instance.lazarus.public_ip
+}
+
