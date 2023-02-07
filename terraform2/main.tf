a77f9526a3343df877c3f66eb279fd41d02b0789
ej838639 <eric.johnson838639@gmail.com>
2023-02-07 13:44:22 -0800

    devops: LAZ-8 main.tf added private key

--- terraform2/main.tf
+++ terraform2/main.tf
@@ -20,10 +20,25 @@ locals {
 resource "aws_instance" "lazarus" {
   ami           = local.linux_os
   instance_type = local.smallest_instance
+  key_name = "tf-key-pair"
 
   tags = {
     Name = "lazarus"
   }
+}
+
+resource "aws_key_pair" "tf-key-pair" {
+  key_name = "tf-key-pair"
+  public_key = tls_private_key.rsa.public_key_openssh
+}
+
+resource "tls_private_key" "rsa" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+resource "local_file" "tf-key" {
+  content  = tls_private_key.rsa.private_key_pem
+  filename = "tf-key-pair"
 }
 
 output "instance_public_ip" {
