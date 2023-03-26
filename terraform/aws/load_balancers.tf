cc4e74d8a161260417acf515e477e04bc9a848f0
ej838639 <eric.johnson838639@gmail.com>
2023-03-25 18:37:11 -0700

    feat LAZ-9 Terraform create network load balancer, target group, add listener- WORKS to https://sntxrr.org

--- terraform/aws/load_balancers.tf
+++ terraform/aws/load_balancers.tf
@@ -110,3 +110,93 @@ resource "aws_route53_record" "www" {
   ttl     = 300
   records = [aws_eip.eip_b.public_ip,aws_eip.eip_c.public_ip]
 }
+
+resource "aws_lb" "load_balancer_nlb" {
+  name               = local.load_balancer_nlb
+  internal           = false
+  load_balancer_type = "network"
+
+  subnet_mapping {
+    subnet_id = "${aws_default_subnet.default_2b.id}"
+    allocation_id = aws_eip.eip_b.id
+  }
+
+  subnet_mapping {
+    subnet_id = "${aws_default_subnet.default_2c.id}"
+    allocation_id = aws_eip.eip_c.id
+  }
+
+  tags = {
+    Key=local.project
+  }
+}
+
+resource "aws_lb_target_group" "target_group_nlb_http" {
+  name        = local.target_group_nlb_http
+  target_type = "alb"
+  protocol    = "TCP"
+  port        = 80
+  vpc_id      = aws_default_vpc.default.id
+
+  health_check {
+    enabled = true
+    protocol = "HTTP"
+  }
+
+  tags = {
+    Key=local.project
+  }
+}
+
+resource "aws_lb_target_group" "target_group_nlb_https" {
+  name        = local.target_group_nlb_https
+  target_type = "alb"
+  protocol    = "TCP"
+  port        = 443
+  vpc_id      = aws_default_vpc.default.id
+
+  health_check {
+    enabled = true
+    protocol = "HTTPS"
+  }
+
+  tags = {
+    Key=local.project
+  }
+}
+
+resource "aws_lb_target_group_attachment" "tg_attachment_nlb_http" {
+    target_group_arn = aws_lb_target_group.target_group_nlb_http.arn
+    target_id        = aws_lb.load_balancer_alb.id
+}
+
+resource "aws_lb_target_group_attachment" "tg_attachment_nlb_https" {
+    target_group_arn = aws_lb_target_group.target_group_nlb_https.arn
+    target_id        = aws_lb.load_balancer_alb.id
+}
+
+resource "aws_lb_listener" "listener_http_nlb" {
+  load_balancer_arn = aws_lb.load_balancer_nlb.arn
+  port              = "80"
+  protocol          = "TCP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.target_group_nlb_http.arn
+  }
+}
+
+resource "aws_lb_listener" "listener_https_nlb" {
+  load_balancer_arn = aws_lb.load_balancer_nlb.arn
+  port              = "443"
+  protocol          = "TCP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.target_group_nlb_https.arn
+  }
+}
+
+output "hyperlink_sntxrr" {
+  value = "https://sntxrr.org"
+}
