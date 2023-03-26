4e053c3c4a300c9ab808b11c9db8af37d52ad196
ej838639 <eric.johnson838639@gmail.com>
2023-03-25 19:08:19 -0700

    Remove microservice architecture folder

--- terraform/aws_microservice_architecture/config.tf
+++
@@ -1,6 +0,0 @@
-# This configuration file will contain the provider configurations and tested versions.
-# Specify the Terraform provider version.
-# Configure the AWS Provider
-provider "aws" {
-  region = "us-west-2"
-}
--- terraform/aws_microservice_architecture/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "alb_dns_name" {
-  description = "The Application Load Balancer DNS name"
-  value       = aws_lb.main.*.dns_name[0]
-}
